cabb34218af89cd90c0a88fc98fffe4defd823f1
Erin Becker <erinstellabecker@gmail.com>
2020-07-27 19:21:47 -0700

    add opacity, borders, color palette

--- mondrianize2.R
+++ mondrianize2.R
@@ -6,18 +6,20 @@ library(ggplot2)
 
 # random mondrian with random data
 
-random_mondrian <- function(boxes, color_palette = NULL, opacity = TRUE) {
+random_mondrian <- function(boxes, color_palette = NULL, 
+                            opacity = TRUE, borders = FALSE) {
   
   # set default color palette if none provided
   if(is.null(color_palette)) {
-    color_palette <- c("red", "blue", "white", "black", "yellow")
+    color_palette <- c("red", "blue", "white", "black", "yellow", 
+                       "darkgreen", "darkblue")
   }
 
-  # generate colors for plot by sampling from palette
-  colors_rand <- sample(color_palette, boxes, replace = TRUE)
-  
+  # generate fill colors for plot by sampling from palette
+  fill_rand <- sample(color_palette, boxes, replace = TRUE)
+    
   # initialize dataframe to store random values of dimensions
-  data_rand <- data.frame(color = colors_rand, alpha = 1)
+  data_rand <- data.frame(fill = fill_rand, alpha = 1)
   
   # list the dimensions that are going to be randomly generated
   dimensions <- c("xmin_rand", "xmax_rand", "ymin_rand", "ymax_rand")
@@ -29,34 +31,65 @@ random_mondrian <- function(boxes, color_palette = NULL, opacity = TRUE) {
   for(i in dimensions) data_rand[[i]] = runif(boxes, 0, 1)
   
   # make the plot
-  ggplot() + 
+  plot <- ggplot() + 
     geom_rect(data_rand, mapping = 
                 aes(xmin = xmin_rand, xmax = xmax_rand, 
                     ymin = ymin_rand, ymax = ymax_rand),
-              fill = data_rand$color,
+              fill = data_rand$fill,
               alpha = data_rand$alpha) +
     theme_void() + 
     theme(plot.background = element_rect(color = "black"))
+  
+  # add border lines if borders = TRUE
+  if(borders == TRUE) {
+    
+    # add border variables to data
+    data_rand$vbord <- sample(c(TRUE, FALSE, FALSE, FALSE), nrow(data_rand), 
+                              replace = TRUE)
+    data_rand$hbord <- sample(c(TRUE, FALSE, FALSE, FALSE), nrow(data_rand),
+                              replace = TRUE)
+    
+    # add lines to plot
+    plot <- plot +
+      geom_hline(yintercept = 
+                    data_rand[which(data_rand$hbord == TRUE),]$ymin_rand, 
+                    color = "black") +
+      geom_vline(xintercept = 
+                   data_rand[which(data_rand$vbord == TRUE),]$xmin_rand,
+                 color = "black")
+  }
+  plot
 }
 
-
-
-
-# my_colors <- c("red", "blue", "white", "black", "yellow")
-# colors_rand <- sample(my_colors, 10, replace = TRUE)
-# 
-# data_rand <- data.frame(color = colors_rand)
-# 
-# dimensions <- c("xmin_rand", "xmax_rand", "ymin_rand", "ymax_rand",
-#                 "alpha")
-# 
-# for(i in dimensions) data_rand[[i]] = runif(10, 0, 1)
-# 
-# ggplot() + 
-#   geom_rect(data_rand, mapping = 
-#               aes(xmin = xmin_rand, xmax = xmax_rand, 
-#                   ymin = ymin_rand, ymax = ymax_rand),
+# random_mondrian <- function(boxes, color_palette = NULL, opacity = TRUE) {
+#   
+#   # set default color palette if none provided
+#   if(is.null(color_palette)) {
+#     color_palette <- c("red", "blue", "white", "black", "yellow")
+#   }
+#   
+#   # generate colors for plot by sampling from palette
+#   colors_rand <- sample(color_palette, boxes, replace = TRUE)
+#   
+#   # initialize dataframe to store random values of dimensions
+#   data_rand <- data.frame(color = colors_rand, alpha = 1)
+#   
+#   # list the dimensions that are going to be randomly generated
+#   dimensions <- c("xmin_rand", "xmax_rand", "ymin_rand", "ymax_rand")
+#   
+#   # add alpha as variable if opacity is TRUE
+#   if(opacity == TRUE) dimensions <- c(dimensions, "alpha")
+#   
+#   # generate random values of dimensions
+#   for(i in dimensions) data_rand[[i]] = runif(boxes, 0, 1)
+#   
+#   # make the plot
+#   ggplot() + 
+#     geom_rect(data_rand, mapping = 
+#                 aes(xmin = xmin_rand, xmax = xmax_rand, 
+#                     ymin = ymin_rand, ymax = ymax_rand),
 #               fill = data_rand$color,
-#             alpha = data_rand$alpha) +
-#   theme_void() + 
-#   theme(plot.background = element_rect(color = "black"))
+#               alpha = data_rand$alpha) +
+#     theme_void() + 
+#     theme(plot.background = element_rect(color = "black"))
+# }
